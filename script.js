1a29721af7454b8381fe157deaea5f83f83ee76f
LizetteTrejo <lizette.trejo@hotmail.com>
2024-09-10 13:34:18 -0600

    feat: Imprimir el resultado en el paragraph

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ function leerValorDeNombre(){
 }
 
 function imprimirResultado( resultado ){
-    console.log( resultado );
+    const paragraph = document.getElementById("vocales");
+    paragraph.innerHTML = resultado;
    }
 
 function manejoDelEventoInputNombre(){
